029ce266259850c1d3fe63b09f53a5b11b062d0c
hamidreza.abooei <hamidreza.abooei@gmail.com>
2021-05-26 22:00:01 +0430

    [implement] player abstract

--- Server/src/org/ap/midterm/Models/GameManager.java
+++ Server/src/org/ap/midterm/Models/GameManager.java
@@ -3,5 +3,5 @@ package org.ap.midterm.Models;
 public class GameManager {
     GameState gameState;
     GameLoop gameLoop;
-    
+
 }
--- Server/src/org/ap/midterm/Models/Player.java
+++ Server/src/org/ap/midterm/Models/Player.java
@@ -1,7 +1,37 @@
 package org.ap.midterm.Models;
 
 public abstract class Player{
-    boolean alive = true;
+    protected boolean alive = true;
+    protected String name;
 
+    /**
+     * getter
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
 
+    /**
+     * setter
+     * @param userName set name
+     */
+    public void setName(String userName) {
+        this.name = userName;
+    }
+
+    /**
+     * die player
+     */
+    public void die(){
+        alive = false;
+    }
+
+    /**
+     * get live status
+     * @return alive
+     */
+    public boolean isAlive(){
+        return alive;
+    }
 }
