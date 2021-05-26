4f5fa90bd1789246314daa7da4ceeb0b7b989dfe
hamidreza.abooei <hamidreza.abooei@gmail.com>
2021-05-27 00:21:20 +0430

    [implement] Non Thread server

--- Server/src/org/ap/midterm/ui/Server.java
+++ Server/src/org/ap/midterm/ui/Server.java
@@ -2,9 +2,23 @@ package org.ap.midterm.ui;
 
 import org.ap.midterm.Models.GameManager;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     GameManager gameManager;
     public Server(int port){
+        try (ServerSocket serverSocket = new ServerSocket(8080);){
+            Socket socket = serverSocket.accept();
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            
 
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        }
     }
 }
