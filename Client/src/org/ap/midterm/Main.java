ae3eb47ce039eaae9422c6a5342399368b536c0e
hamidreza.abooei <hamidreza.abooei@gmail.com>
2021-05-26 21:04:49 +0430

    [Add] Client

---
+++ Client/src/org/ap/midterm/Main.java
@@ -0,0 +1,8 @@
+package org.ap.midterm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Client/src/org/ap/midterm/ui/Client.java
@@ -0,0 +1,4 @@
+package org.ap.midterm.ui;
+
+public class Client {
+}
