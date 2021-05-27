433e4847a19eb473ef6761d54ccebc380e9206a3
hamidreza.abooei <hamidreza.abooei@gmail.com>
2021-05-28 02:04:54 +0430

    [implement] client main

--- Client/src/org/ap/midterm/Main.java
+++ Client/src/org/ap/midterm/Main.java
@@ -1,8 +1,12 @@
 package org.ap.midterm;
 
+import org.ap.midterm.ui.Client;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Client client = new Client();
+        client.startClient("127.0.0.1" , 8080);
+
     }
 }
