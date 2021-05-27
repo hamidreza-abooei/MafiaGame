5b5e66777c2dc21a53c849c41b7c22712b000d66
hamidreza.abooei <hamidreza.abooei@gmail.com>
2021-05-28 00:52:34 +0430

    [implement] Client Handler

--- Server/src/org/ap/midterm/ui/ClientHandler.java
+++ Server/src/org/ap/midterm/ui/ClientHandler.java
@@ -1,4 +1,32 @@
 package org.ap.midterm.ui;
 
-public class ClientHandler {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+
+    private int clientID;
+    private Socket connection;
+    public ClientHandler(Socket connection , int clientID){
+        this.clientID = clientID;
+        this.connection = connection;
+    }
+    @Override
+    public void run() {
+        try (DataInputStream in = new DataInputStream(connection.getInputStream());
+             DataOutputStream out = new DataOutputStream(connection.getOutputStream())){
+            while (true){
+                String input = in.readUTF();
+                if( input.equalsIgnoreCase("end")){
+                    break;
+                }
+            }
+            System.out.println("Server closed	 [Client-" + clientID + "] connection");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
