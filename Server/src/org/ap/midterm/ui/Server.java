19c528ebe3bb067b548938c504b5cb4465beb77d
hamidreza.abooei <hamidreza.abooei@gmail.com>
2021-05-28 00:44:56 +0430

    [implement] Server Multithread

--- Server/src/org/ap/midterm/ui/Server.java
+++ Server/src/org/ap/midterm/ui/Server.java
@@ -7,18 +7,34 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     GameManager gameManager;
-    public Server(int port){
-        try (ServerSocket serverSocket = new ServerSocket(8080);){
-            Socket socket = serverSocket.accept();
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            
+    private boolean running;
 
+    public Server(int port){
+        this.running = true;
+    }
+
+    public void startServer( int port ){
+        ExecutorService pool = Executors.newCachedThreadPool();
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Server with port : " + port + " Started \nWaiting for Client .....");
+            int clientNumber = 1;
+            while (running){
+                pool.execute(new ClientHandler(serverSocket.accept(),clientNumber));
+                System.out.println("Server connected to new Client [Client-" + clientNumber + "]");
+                clientNumber++;
+            }
+            pool.shutdown();
         } catch (IOException e) {
             System.err.println(e.toString());
         }
     }
+    public void downServer(){
+        this.running = false;
+    }
+
 }
