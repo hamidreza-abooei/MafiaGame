63bccc4b0a74151bf2daf97f8fbba0d6125ef941
hamidreza.abooei <hamidreza.abooei@gmail.com>
2021-06-15 19:13:37 +0430

    [ReDesign] GameRule

--- Server/src/org/ap/midterm/Models/GameRules.java
+++ Server/src/org/ap/midterm/Models/GameRules.java
@@ -34,40 +34,67 @@ public class GameRules {
             if (gameState.getPlayer("DrLecter").isAlive()){
                 ((Mafia)gameState.getPlayer("DrLecter")).setKiller();
             }else{
+                // if the number of players become more than 10, it should be change a little bit
                 ((Mafia)gameState.getPlayer("Mafia")).setKiller();
             }
         }
     }
+
+    /**
+     * add one event
+     * @param sendUsername A username that send this message
+     * @param message the message (event)
+     */
     public void addEvent(String sendUsername , String message){
         boolean addOrNot = true;
-
-        if (gameState.getGameMode()==GameMode.NIGHT){
-            try {
-                int usernameID = Integer.parseInt(message);
-                ArrayList<String> choices = gameManager.getAliveCitizens();
-                message = choices.get(usernameID);
-                if (!sendUsername.equalsIgnoreCase(gameState.getKillerUsername())) {
-                    // this part is for Dr.Lecter that can do two things in one night
-                    boolean kill = true; // between kill and save
-                    for(String eventSender:eventSenders){
-                        if (eventSender.equalsIgnoreCase(sendUsername)){
-                            kill = false;
-                        }
-                    }
-                    if(kill){
-                        gameManager.sendMessageToKiller(sendUsername + " opinion is to kill " + message);
+        int usernameID = -1;
+        if (gameState.getGameMode() == GameMode.NIGHT){
+            usernameID = toInteger(sendUsername , message);
+            ArrayList<String> choices = gameManager.getAliveCitizens();
+            message = choices.get(usernameID);
+            if (!sendUsername.equalsIgnoreCase(gameState.getKillerUsername())) {
+                // this part is for Dr.Lecter that can do two things in one night
+                boolean kill = true; // between kill and save
+                for(String eventSender:eventSenders){
+                    if (eventSender.equalsIgnoreCase(sendUsername)){
+                        kill = false;
                     }
                 }
-            }catch (NumberFormatException e){
-                gameManager.sendMessageToClientHandler(sendUsername,"wrong input");
-                gameManager.sendMessageToClientHandler(sendUsername,"read");
-                addOrNot = false;
+                if(kill){
+                    gameManager.sendMessageToKiller(sendUsername + " opinion is to kill " + message);
+                }
             }
         }
-        if (addOrNot) {
+
+        if (gameState.getGameMode() == GameMode.DAY){
+            usernameID = toInteger(sendUsername , message);
+            ArrayList<String> choices = gameState.getAliveUsernames();
+            message = choices.get(usernameID);
+        }
+
+        if (usernameID != -1) {
             eventSenders.add(sendUsername);
             events.add(message);
 //            System.out.println("event Added user:" + sendUsername + " event " + message );
         }
+    }
+
+
+    /**
+     * to handle exception
+     * @param sendUsername username
+     * @param message message to integer
+     * @return integer of message
+     */
+    private int toInteger(String sendUsername , String message){
+        int usernameID ;
+        try{
+            usernameID = Integer.parseInt(message);
+            return usernameID ;
+        }catch (NumberFormatException e){
+            gameManager.sendMessageToClientHandler(sendUsername,"wrong input");
+            gameManager.sendMessageToClientHandler(sendUsername,"read");
+        }
+        return -1;
     }
 }
